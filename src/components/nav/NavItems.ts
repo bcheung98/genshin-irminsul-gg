044869b30f36b03a81cc0713f0a4affd2e406c0b
Brian <briantcheung@gmail.com>
2025-03-31 13:56:06 -0700

    Update nav

---
+++ src/components/nav/NavItems.ts
@@ -0,0 +1,52 @@
+export interface NavItem {
+    icon: string;
+    text: string;
+    link: string;
+    tag?: string;
+}
+
+export const navItems: NavItem[] = [
+    {
+        icon: "icons/Sumeru",
+        text: "Home",
+        link: "/",
+    },
+    {
+        icon: "icons/Aether",
+        text: "Characters",
+        link: "/characters/",
+    },
+    {
+        icon: "icons/Weapons",
+        text: "Weapons",
+        link: "/weapons/",
+    },
+    {
+        icon: "icons/Artifact",
+        text: "Artifacts",
+        link: "/artifacts/",
+    },
+    {
+        icon: "icons/Ascension",
+        text: "Ascension Planner",
+        link: "/planner/",
+    },
+    {
+        icon: "icons/Wish",
+        text: "Banner Archive",
+        link: "/banners/",
+    },
+];
+
+export const otherItems: NavItem[] = [
+    {
+        text: "Calendar",
+        link: "https://irminsul.gg/calendar",
+        icon: "",
+    },
+    {
+        text: "Blog",
+        link: "https://irminsul.gg/blog",
+        icon: "",
+    },
+];
