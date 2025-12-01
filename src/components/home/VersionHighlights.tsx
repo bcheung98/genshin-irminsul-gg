942e224a03d5816cdc72ed7f379cce3afd354f16
Brian <briantcheung@gmail.com>
2025-12-01 11:59:26 -0800

    Adjust width

--- src/components/home/VersionHighlights.tsx
+++ src/components/home/VersionHighlights.tsx
@@ -125,7 +125,7 @@ function VersionHighlights() {
                         label="Version"
                         onChange={handleIndexChange}
                         input={<StyledInput />}
-                        sx={{ mx: "4px", width: "96px" }}
+                        sx={{ mx: "4px", width: "104px" }}
                     >
                         {updates.map((version, index) => (
                             <StyledMenuItem key={index} value={index}>
